c58f41a953bf790246ee7497ddcaf49a8a5de4aa
kanhaiya kumar <kk7077574@gmail.com>
2025-08-27 23:37:25 +0530

    selection sort implemented

---
+++ Practice/7_selectionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the Size of Array :";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the Elements of array :";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
