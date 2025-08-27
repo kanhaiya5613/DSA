07d31a11c136c171d975b0e44ff9bcbaf877d0f3
kanhaiya kumar <kk7077574@gmail.com>
2025-08-27 18:16:53 +0530

    Palendrome number code added

--- Practice/4_Palendrome.cpp
+++ Practice/4_Palendrome.cpp
@@ -1,8 +1,22 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int num;
-//     cout<< "Enter a Number :";
-//     cin>> num;
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<< "Enter a Number :";
+    cin>> num;
+        int num1 = num;
+    int reverse=0;
+    while(num>0){
+        int temp = num%10;
+        reverse = reverse*10+temp;
+        num = num/10;
+    }
+    cout<<reverse << endl;
 
-// }
+    if(reverse==num1){
+        cout<<"it  is a palendrome number";
+    }
+    else{
+        cout <<"it is not a palendrome number";
+    }
+}
