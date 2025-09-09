3432d82834e8001a20c6b7197f87c3f52ee0d757
kanhaiya kumar <kk7077574@gmail.com>
2025-09-09 21:55:07 +0530

    Implement setMatrix function to set matrix rows and columns to zero based on input

---
+++ Practice/29_setMatrixZero_Better.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int setMatrix(int **arr, int n, int m)
+{
+    int row[n] = {0};
+    int col[m] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == 0)
+            {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] == 1 || col[j] == 1)
+            {
+                arr[i][j] = 0;
+            }
+        }
+    }
+    cout << "Final matrix is : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    int m;
+    cout << "Enter the numbers of rows and columns respectively : ";
+    cin >> n >> m;
+    int **arr = new int *[n];
+    ;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[m];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    setMatrix(arr, n, m);
+}
