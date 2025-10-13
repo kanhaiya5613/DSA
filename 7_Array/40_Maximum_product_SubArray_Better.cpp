6a20816fb2640ff02d850495ad259332aaf6b616
kanhaiya kumar <kk7077574@gmail.com>
2025-10-13 22:39:39 +0530

    Implemented the better approach of the probleum Maximum Product Subarray

---
+++ 7_Array/40_Maximum_product_SubArray_Better.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int MaximumProductSubarray(int arr[],int n,int product){
+    int count = 0;
+    int mult;
+    for(int i=0;i<n;i++){
+        mult = arr[i];
+        for(int j=i+1;j<n;j++){
+           mult *= arr[j];
+            if(mult == product){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int main(){
+    int n;
+    cout << "Enter the size of array : ";
+    cin >> n;
+    int product;
+    cout << "Enter the value of Product : ";
+    cin >> product;
+    int arr[n];
+    cout << "Enter the elements of array : ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout<< "The maximum count of subarray is : "<<MaximumProductSubarray(arr,n,product);
+}
