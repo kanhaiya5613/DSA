cd69bcef50bf6e47fd5199c31d21f2692b87b89c
kanhaiya kumar <kk7077574@gmail.com>
2025-11-03 22:03:59 +0530

    Add implementation of next permutation algorithm using brute force approach

---
+++ 7_Array/36_next_permutation_brute.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+    vector<int> nextPermutation(vector<int>& nums) {
+        vector<vector<int>> all;
+        sort(nums.begin(), nums.end());
+        do {
+            all.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        for (int i = 0; i < all.size(); i++) {
+            if (all[i] == nums) {
+                if (i == all.size() - 1)
+                    return all[0];
+                return all[i + 1];
+            }
+        }
+
+        return nums;
+    }
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<int> result = nextPermutation(nums);
+    for (int x : result) cout << x << " ";
+    cout << endl;
+    return 0;
+}
