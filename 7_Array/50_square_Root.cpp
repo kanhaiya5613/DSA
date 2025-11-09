cb7477ee250e428a457c54f68a8118cce4ab22f3
kanhaiya kumar <kk7077574@gmail.com>
2025-11-09 23:37:15 +0530

    Add implementation of square root function in C++

---
+++ 7_Array/50_square_Root.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mySqrt(int n){
+    return sqrt(n);
+}
+int main(){
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    cout << "Square Root is : " << mySqrt(n);
+}
