97d88f95a83500be23e8360d38fcb3a3fc27d88f
kanhaiya kumar <kk7077574@gmail.com>
2025-11-15 23:49:34 +0530

    **Add implementation of linked list node and insertion function in C++**

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+class Node {
+    public:
+    int data;
+    Node* next;
+    // constructor
+    Node(int data){
+        this Data = data;
+        next = null ptr;
+    }
+};
+void insertionlist(int data) {
+    Node* ptr = new Node(data)
+    if(Head == nullptr){
+        Head=Tail=ptr;
+    }
+    else{
+        ptr=next=Head;
+        Head= ptr;
+        count++;
+    }
+}
