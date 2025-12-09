42ec741a1b8cbe2b67d83b7c710f004d32de59e8
kanhaiya kumar <kk7077574@gmail.com>
2025-12-10 02:16:32 +0530

    add optimal approach implimentation of finding single element from array using binary search

---
+++ 7_Array/56_single_number_in_sorted_array_optimal.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int singleNonDuplicate(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+    {
+        return nums[0];
+    }
+
+    if (nums[0] != nums[1])
+    {
+        return nums[0];
+    }
+    if (nums[n - 1] != nums[n - 2])
+    {
+        return nums[n - 1];
+    }
+
+    int low = 1;
+    int high = n - 2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        {
+            return nums[mid];
+        }
+        if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of array : ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    cout << "Single Number is : " << singleNonDuplicate(nums);
+}
