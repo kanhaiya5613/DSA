ca526c55fcf1b75ab429c8145519866a2f7a9132
kanhaiya kumar <kk7077574@gmail.com>
2025-12-17 02:17:47 +0530

    Add implementation to calculate the sum of digits until the result is less than 10 in C++

---
+++ 7_Array/61_Add_Digits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int addDigits(int num) {
+        while(num>=10){
+            int sum = 0;
+            while(num>0){
+                sum += num%10;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+    int main(){
+        int n;
+        cout << "Enter a value : ";
+        cin >> n;
+        cout << "The sum of all digits till sum in not less then 10 is : "<< addDigits(n);
+    }
