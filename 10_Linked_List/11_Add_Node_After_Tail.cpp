c1a903dae6f3130ac9f8b7f5178561badacf2269
kanhaiya kumar <kk7077574@gmail.com>
2026-01-04 02:10:09 +0530

    Add implementation to handle adding a node  after the tail in a linked list

--- 10_Linked_List/11_Add_Node_After_Tail.cpp
+++ 10_Linked_List/11_Add_Node_After_Tail.cpp
@@ -28,20 +28,25 @@ Node* ConvertArrToLL(vector<int>&arr){
     }
     return head;
 }
-Node* AddNodeBeforeHead(Node* head,int k){
+Node* AddNodeAfterTail(Node* head,int k){
     if(head==NULL){
         return new Node(k);
     }
-    Node* temp = new Node(k,head);
-    
-    return temp;
+    Node* temp1 = head;
+    while(temp1->next !=nullptr){
+        temp1 = temp1->next;
+    }
+    Node* temp = new Node(k);
+    temp1->next = temp;
+
+    return head;
 }
 
 int main(){
     vector<int> arr = {1,4,6,9,11};
     int k = 11;
     Node* head = ConvertArrToLL(arr);
-    Node* head1 = AddNodeBeforeHead(head,k);
+    Node* head1 = AddNodeAfterTail(head,k);
     Node* temp = head1;
     while(temp){
         cout << temp->data << " ";
