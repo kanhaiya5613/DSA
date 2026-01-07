9c3ba6ced6112129bae3817970c46f57f7ab7ca2
kanhaiya kumar <kk7077574@gmail.com>
2026-01-07 17:23:26 +0530

    Implementation of Delete Kth Element in DLL

---
+++ 10_Linked_List/Dubly_Linked_List/4_Delete_Kth_Node.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1,Node* next1, Node* back1){
+        data = data1;
+        next=next1;
+        back = back1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+Node* convertArrayToDLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+void print(Node* head){
+    while(head != nullptr){
+        cout << head-> data << " ";
+        head = head->next;
+    }
+}
+Node* deleteTail(Node* head){
+    if(head==NULL || head->next == NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    while(prev->next != nullptr){
+         prev = prev->next;
+    }
+    if(prev->next == nullptr){
+        prev->back->next = nullptr;
+        prev->back = nullptr;
+        
+    }
+    delete prev;
+    return head;
+}
+Node* deleteHead(Node* head){
+    if(head==NULL || head->next == NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head = head->next;
+
+    head->back = nullptr;
+    prev->next = nullptr;
+
+    delete prev;
+    return head;
+}
+Node* removeKthElement(Node* head,int k){
+    if(head==nullptr){
+        return nullptr;
+    }
+    int cnt = 0;
+    Node* kNode = head;
+    while(kNode != nullptr){
+        cnt++;
+        if(cnt==k) break;
+        kNode = kNode->next;
+    }
+    Node* prev = kNode->back;
+    Node* front = kNode->next;
+
+    if(prev == nullptr && front == nullptr){
+        return nullptr;
+    }
+    else if(prev==nullptr){
+        return deleteHead(head);
+    }
+    else if(front == nullptr){
+        return deleteTail(head);
+    }
+    prev->next = front;
+    front->back = prev;
+
+    kNode->next = nullptr;
+    kNode->back = nullptr;
+    return head;
+}
+int main(){
+    vector<int> arr = {12,8,6,4,9,7};
+    int k = 3;
+    Node* head = convertArrayToDLL(arr);
+    head = removeKthElement(head,k);
+    print(head);
+    return 0;
+}
