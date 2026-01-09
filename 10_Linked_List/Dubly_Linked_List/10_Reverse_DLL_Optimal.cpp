6c0fe2d595c32f39adbcd7e268bb0560cb6b1779
kanhaiya kumar <kk7077574@gmail.com>
2026-01-10 03:32:24 +0530

    Add optimal approach implementation of Reverse a DLL

---
+++ 10_Linked_List/Dubly_Linked_List/10_Reverse_DLL_Optimal.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *back;
+
+public:
+    Node(int data1, Node *next1, Node *back1)
+    {
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+Node *convertArrayToDLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node* ReverseDLL(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return head;
+    }
+    Node* prev = nullptr;
+    Node* current = head;
+    while(current != nullptr){
+        prev = current-> back;
+        current->back = current->next;
+        current->next=prev;
+        current = current->back;
+    }
+    return prev->back;
+
+}
+int main()
+{
+    vector<int> arr = {12, 8, 6, 4, 9, 7};
+    Node *head = convertArrayToDLL(arr);
+    head = ReverseDLL(head);
+    print(head);
+    return 0;
+}
