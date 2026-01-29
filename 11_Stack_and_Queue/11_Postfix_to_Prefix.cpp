cfe0bd5561e0b721cc8463fd384043d32cc392f7
kanhaiya kumar <kk7077574@gmail.com>
2026-01-29 11:00:25 +0530

    Add implementation of Postfix to Prefix conversion

---
+++ 11_Stack_and_Queue/11_Postfix_to_Prefix.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+string PostfixToPrefix(string s){
+    stack<string>st;
+    int i = 0;
+    while(i<s.size()){
+        if((s[i]>='0' && s[i]<='9' ) || (s[i]>='a' && s[i]<='z') || (s[i] >='A' && s[i]<='Z') ){
+           st.push(string(1, s[i]));
+
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string con = string(1,s[i]) + t2 + t1;
+            st.push(con);
+        }
+        i++;
+    }
+    return st.top();
+}
+int main(){
+    string s;
+    cout << "Enter Postfix Expression : ";
+    getline(cin,s);
+
+    string Infix = PostfixToPrefix(s);
+
+    cout << "Prefix Expression : " << Infix;
+}
